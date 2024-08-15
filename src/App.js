a1fee784ffcf97dde5b14a5a975cf3456943ace3
yura <espoir3045@gmail.com>
2024-08-15 20:31:13 +0900

    파일명 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './css/style.scss';
 import { useEffect, useState } from 'react';
 import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
-// import Main from './pages/main.jsx';
+import Main from './pages/main.js';
 
 export default function App() {
   return (
