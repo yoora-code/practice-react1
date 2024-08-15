12a351c84c2f902f54936669005417ac405d663c
yura <espoir3045@gmail.com>
2024-08-15 18:53:01 +0900

    first commit - src

---
+++ shopping-site-1/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './css/style.scss';
+import { useEffect, useState } from 'react';
+import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
+import Main from './pages/main';
+
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
