c95d5fe34e1d2930065d9dce2f36f4638997438d
yura <espoir3045@gmail.com>
2024-08-15 19:46:02 +0900

    파일명 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './css/style.scss';
 import { useEffect, useState } from 'react';
 import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
-import Main from './pages/main';
+import Main from './pages/main.jsx';
 
 function App() {
   return (
