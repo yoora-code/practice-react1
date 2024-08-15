cff91947ca1818f46c7488a2c57a2b5be757c5c3
yura <espoir3045@gmail.com>
2024-08-15 20:26:43 +0900

    ㅇㄴㄹㄴㅇㄹ

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './css/style.scss';
 import { useEffect, useState } from 'react';
 import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
-import Main from './pages/main.jsx';
+// import Main from './pages/main.jsx';
 
 function App() {
   return (
     <div>
-      <Main/>
+      왜 안돼냐
     </div>
   );
 }
