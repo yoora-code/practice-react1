0f1c49cedd697edb0231e0fa6a628f876384d296
yr <yr817@hivelab.co.kr>
2024-08-16 17:23:32 +0900

    C:/Program Files/Git/

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './css/style.scss';
-import { useEffect, useState } from 'react';
-import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
-import Main from './pages/main.js';
+// import { useEffect, useState } from 'react';
+// import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
+// import Main from './pages/main.js';
 
 export default function App() {
   return (
